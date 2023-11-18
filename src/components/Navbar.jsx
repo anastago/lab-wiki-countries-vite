06dd2646f773827893b8cab66dcece47383b86a6
An Go <gorbunoochek@gmail.com>
2023-11-18 16:14:31 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,16 @@
-function Navbar() {}
+import React from "react"
+import { Link } from "react-router-dom"
 
-export default Navbar;
+function Navbar(props) {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          WikiCountries
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
